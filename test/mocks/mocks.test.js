27ec9398dbc9628c5be1c96c90491173ebb33fda
CALIARI Axel <axel.caliari@efrei.net>
2019-12-24 11:31:57 +0100

    test

--- test/mocks/mocks.test.js
+++ test/mocks/mocks.test.js
@@ -6,17 +6,11 @@ const html = fs.readFileSync(
 );
 const test = require("../../st2tst_js");
 
-describe("Birth date", () => {
-  beforeEach(() => {
-    document.documentElement.innerHTML = html.toString();
-  });
-
-  it("Select created and options added", function() {
-    const mockFunction = jest.fn(test.setJourDateDeNaissance);
-    mockFunction();
-    expect(mockFunction.mock.calls.length).toBe(1);
-
-    expect(document.getElementById("jDn")).toBeTruthy();
-    expect(document.getElementById("jDn").children).toBeTruthy();
-  });
-});
+/*   
+  checkBirth,
+  checkMineur,
+  checkNumTelephone,
+  checkCodePostal,
+  checkCityDijon,
+  checkEmail 
+*/
